042bca07ca2bef519cba314c6203e727e220cc5f
Ivan <mayhemk57@gmail.com>
2025-07-28 22:10:37 -0600

    home page bones set up

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -17,11 +17,11 @@ watch(theme, () => {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md bg-codeworks border-bottom border-vue">
+  <nav class="navbar navbar-expand-md bg-dark border-bottom border-4 border-success">
     <div class="container gap-2">
       <RouterLink :to="{ name: 'Home' }" class="d-flex align-items-center text-light">
-        <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" />
-        <b class="fs-5">Vue Starter</b>
+        <b class="mdi mdi-cards fs-2 me-2"></b>
+        <b class="fs-5">Card Store</b>
       </RouterLink>
       <!-- collapse button -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,13 +1,56 @@
-<script setup >
-import Example from '@/components/Example.vue';
+<script setup>
 
 
 </script>
 
 <template>
-  <Example />
+  <section class="container-fluid g-0">
+    <div class="row ">
+      <div class="col-12">
+        <div class="outline-font fs-1 bg-gray text-center fw-bold p-3">headline</div>
+      </div>
+      <div class="container-fluid">
+        <div class="row text-center modak fs-1 p-2 bg-warning">
+          <div class="col-md-6 col-12">
+            Card Options
+          </div>
+          <div class="col-md-6 col-12">
+            Card Market
+          </div>
+        </div>
+        <div class="row text-center modak fs-1 p-2">
+          <div class="col-md-6 col-12">
+            <img src="../assets/img/cards.svg" alt="Pokemon Card Back Image" class="card"
+              title="Back of a Pokemon card">
+          </div>
+          <div class="col-md-6 col-12">
+            <img src="https://a.pinatafarm.com/1858x1304/ff543bb12a/stonks-without-stonks.jpg" alt="" class="img">
+          </div>
+        </div>
+        <div class="row text-center modak fs-1 p-2">
+          <div class="col-md-6 col-12">
+            <img src="../assets/img/cards.svg" alt="Magic Card Back Image" class="card"
+              title="Back of a Magic The Gathering card">
+          </div>
+          <div class="col-md-6 col-12">
+            <img src="https://a.pinatafarm.com/1858x1304/ff543bb12a/stonks-without-stonks.jpg" alt="" class="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style scoped lang="scss">
+.card {
+  min-height: 20dvh;
+  max-height: 80dvh;
+}
 
+.img {
+  object-fit: cover;
+  max-width: 100%;
+}
 </style>
+
+https://i.insider.com/601448566dfbe10018e00c5d?width=700&format=jpeg&auto=webp
